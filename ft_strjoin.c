a6030c926f7fb0818c925adc7b696b8c8df3f00e
Tudor Tîrnovan <tudortirnovan@Tudors-MacBook-Air.local>
2025-11-13 16:34:25 +0100

    ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tudortirnovan <tudortirnovan@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 16:04:32 by tudortirnov       #+#    #+#             */
+/*   Updated: 2025/11/13 16:33:01 by tudortirnov      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(const char *s1, const char *s2)
+{
+	char *result;
+	size_t i;
+	size_t x;
+
+	i = 0;
+	x = 0;
+	result = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * 
+	sizeof(char));
+	
+	if(!result)
+		return(NULL);
+
+	while(s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while(s2[x])
+	{
+		result[i + x] = s2[x];
+		x++;
+	}
+	result[i + x] = '\0';
+	return(result);
+}
